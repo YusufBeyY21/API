f7511f9c2f42711ddd7c93cc76ce12164d08088d
YusufBeyY21 <68653852+YusufBeyY21@users.noreply.github.com>
2021-03-05 21:20:26 +0300

    Add files via upload

--- src/services/handlers/ready.handler.ts
+++ src/services/handlers/ready.handler.ts
@@ -22,7 +22,7 @@ export default class ReadyHandler implements EventHandler {
 
         await bot.user.setPresence({
             activity: {
-                name: 'dbots.co',
+                name: 'zbotlist.herokuapp.com',
                 type: 'WATCHING',
                 url: process.env.DASHBOARD_URL
             }            
