5c7de14d04aa4968a8070a50fbe09f75149e10e9
YusufBeyY21 <68653852+YusufBeyY21@users.noreply.github.com>
2021-03-10 16:28:00 +0300

    Update ping.ts

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,7 +1,8 @@
 import { Command, CommandContext, Permission } from './command';
+import { client } from 'discord.js';
 
 export default class HelpCommand implements Command {
     name = 'ping';
     
-    execute = async (ctx: CommandContext) => ctx.channel.send(`🏓 Pong`);
+    execute = async (ctx: CommandContext) => ctx.channel.send(`🏓 Pong ${this.client.ws.ping}`);
 }
